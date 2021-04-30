4932ecd6d4e0314b211dc9f6488024039c47b408
prasad <prasad.athave@iitgn.ac.in>
2021-04-30 23:11:04 +0530

    some changes

--- Q4.py
+++
@@ -1,9 +0,0 @@
-from cv2 import cv2
-path = './image_data/I1/STA_0031.JPG'
-
-img = cv2.imread(path)
-
-print(img[0][1])
-print(img[1][0])
-
-img[0][1] = img[1][0]
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 3DCV_Assignment2
+
+All the code lies in the jupyter notebook 
+
+Comments and variable naming make the code readable
+
